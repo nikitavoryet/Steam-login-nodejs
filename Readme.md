72f092e5a81237363e983ae7b4f56c8cb21c0864
Nikita Vtorushin <n.vtorushin@inbox.ru>
2021-04-14 20:49:43 +0700

    demo task complete

---
+++ Readme.md
@@ -0,0 +1,37 @@
+# Steam profile login with nodejs
+`
+demo work with Steam lib
+`
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run server
+```
+
+#### STEAM RESULT:
+
+`
+{
+eresult: 1,
+out_of_game_heartbeat_seconds: 9,
+in_game_heartbeat_seconds: 9,
+public_ip: 2966522118,
+rtime32_server_time: 1618408064,
+account_flags: 3166213,
+cell_id: 62,
+webapi_authenticate_user_nonce: 'KFxfvZuQ3qZePOO9NYE',
+cell_id_ping_threshold: 2147483647,
+vanity_url: '',
+client_supplied_steamid: '76561199161310515',
+ip_country_code: 'RU',
+count_loginfailures_to_migrate: 0,
+count_disconnects_to_migrate: 0,
+client_instance_id: '2675676925455726589'
+}
+`
+
---
+++ index.js
@@ -0,0 +1,39 @@
+let Steam = require('steam');
+/**
+ * Options demo data for login steam
+ * @type {{password: string, account_name: string}}
+ */
+const logOnOptions = {
+    account_name: "uehsgadpnu02s14",
+    password: 'IDPFP049P85L780'
+};
+const steamClient = new Steam.SteamClient();
+const steamUser = new Steam.SteamUser(steamClient);
+steamClient.connect();
+steamClient.on('connected', () => {
+    steamUser.logOn(logOnOptions);
+});
+steamClient.on('logOnResponse', (logonResp) => {
+    checkerErrorLogin(logonResp).then((res) => {
+        console.log(res)
+    }).catch((e) => {
+        console.log(e)
+    })
+});
+steamClient.on('error', (err) => {
+    console.log(err)
+});
+
+function checkerErrorLogin(payload) {
+   return new Promise(((resolve, reject) => {
+       const code = payload['eresult']
+       if (code === 63) {
+            reject('Account with 2FA email')
+       } else if (code === 85) {
+           reject('Account with steam guard')
+       } else if (code === 5) {
+          reject('ERROR')
+       }
+      resolve(payload)
+   }))
+}
